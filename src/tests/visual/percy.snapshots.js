471fe92afcf84a5276ce1c4bccc5f9a073312d38
boxadesign <info@boxadesign.co.uk>
2021-02-15 14:31:04 +0000

    Testing percy rendering [test-visual]

--- src/tests/visual/percy.snapshots.js
+++ src/tests/visual/percy.snapshots.js
@@ -10,11 +10,17 @@ PercyScript.run(async (page, percySnapshot) => {
   console.log(`Server started at ${testURL}`);
 
   // Get all snapshots of components, patterns and styles
-  const urls = await generateURLs();
-  for (const url of urls) {
-    await page.goto(url.url);
-    await percySnapshot(url.name, { widths: [375, 1300] });
-  }
+  // const urls = await generateURLs();
+  // for (const url of urls) {
+  //   await page.goto(url.url);
+  //   await page.waitFor(1000);
+  //   await percySnapshot(url.name, { widths: [375, 1300] });
+  // }
+
+  // TESTING RENDERING
+  await page.goto(`${testURL}/build/patterns/hub-and-spoke/examples/hub/index.html`);
+  await page.waitFor(1000);
+  await percySnapshot('TESTING PAGE', { widths: [1300] });
 
   // Snapshots of interactive components with state
 
@@ -22,12 +28,14 @@ PercyScript.run(async (page, percySnapshot) => {
   await page.goto(`${testURL}/build/components/checkboxes/examples/checkboxes/index.html`);
   let checkbox = 'input[id="bacon"]';
   await page.evaluate(checkbox => document.querySelector(checkbox).click(), checkbox);
+  await page.waitFor(1000);
   await percySnapshot('Checkbox - checked', { widths: [1300] });
 
   // Radios
   await page.goto(`${testURL}/build/components/radios/examples/radios/index.html`);
   let radio = 'input[id="bacon"]';
   await page.evaluate(radio => document.querySelector(radio).click(), radio);
+  await page.waitFor(1000);
   await percySnapshot('Radio - selected', { widths: [1300] });
 
   // Accordions
@@ -35,6 +43,7 @@ PercyScript.run(async (page, percySnapshot) => {
   page.waitForSelector('.collapsible--initialised');
   let buttonAll = '.js-collapsible-all';
   await page.evaluate(buttonAll => document.querySelector(buttonAll).click(), buttonAll);
+  await page.waitFor(1000);
   await percySnapshot('Accordion - all open', { widths: [1300] });
 
   server.close();
