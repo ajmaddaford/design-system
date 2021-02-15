3a729f64acd01dc2fafa5a053357f0d0dacb7cc2
boxadesign <info@boxadesign.co.uk>
2021-02-15 15:22:47 +0000

    Testing percy rendering [test-visual]

--- src/tests/visual/percy.snapshots.js
+++ src/tests/visual/percy.snapshots.js
@@ -17,25 +17,20 @@ PercyScript.run(async (page, percySnapshot) => {
   //   await percySnapshot(url.name, { widths: [375, 1300] });
   // }
 
-  // TESTING RENDERING
-  await page.goto(`${testURL}/build/patterns/hub-and-spoke/examples/hub/index.html`);
-  await page.waitFor(1000);
-  await percySnapshot('TESTING PAGE', { widths: [1300] });
-
   // Snapshots of interactive components with state
 
   // Checkboxes
   await page.goto(`${testURL}/build/components/checkboxes/examples/checkboxes/index.html`);
   let checkbox = 'input[id="bacon"]';
   await page.evaluate(checkbox => document.querySelector(checkbox).click(), checkbox);
-  await page.waitFor(1000);
+  await page.waitFor(3000);
   await percySnapshot('Checkbox - checked', { widths: [1300] });
 
   // Radios
   await page.goto(`${testURL}/build/components/radios/examples/radios/index.html`);
   let radio = 'input[id="bacon"]';
   await page.evaluate(radio => document.querySelector(radio).click(), radio);
-  await page.waitFor(1000);
+  await page.waitFor(3000);
   await percySnapshot('Radio - selected', { widths: [1300] });
 
   // Accordions
@@ -43,7 +38,7 @@ PercyScript.run(async (page, percySnapshot) => {
   page.waitForSelector('.collapsible--initialised');
   let buttonAll = '.js-collapsible-all';
   await page.evaluate(buttonAll => document.querySelector(buttonAll).click(), buttonAll);
-  await page.waitFor(1000);
+  await page.waitFor(3000);
   await percySnapshot('Accordion - all open', { widths: [1300] });
 
   server.close();
